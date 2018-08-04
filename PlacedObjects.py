ff75ae28f713b685fbfa485ec92335c7347d9b3e
William <wjz1201@columbia.edu>
2018-08-04 18:08:20 -0400

    ready

--- PlacedObjects.py
+++ PlacedObjects.py
@@ -7,6 +7,9 @@ class PlacedObject(object):
         self.x_coord = x_coord
         self.y_coord = y_coord
         self.type = None
+        self.shape = None
+
+    # Setting up the position
 
     def get_x_coord(self):
         return self.x_coord
@@ -22,13 +25,19 @@ class PlacedObject(object):
     def set_reflector(self):
         self.type = "Reflector"
 
+    def set_type(self,in_type):
+        self.type = in_type
+
     def get_type(self):
         return self.type
 
     # Setting up the shape
 
+    def set_shape(self, in_shape):
+        self.shape = in_shape
+
     def get_shape(self):
-        return None
+        return self.shape
 
 class RectangularObject(PlacedObject):
 
@@ -36,7 +45,7 @@ class RectangularObject(PlacedObject):
         super(RectangularObject, self).__init__(x_coord, y_coord)
         self.width = width
         self.height = height
-        self.shape = "Rectangle"
+        self.set_shape("Rectangle")
 
     def get_width(self):
         return self.width
@@ -44,42 +53,37 @@ class RectangularObject(PlacedObject):
     def get_height(self):
         return self.height
 
-    def get_shape(self):
-        return self.shape
-
 
 class CircularObject(PlacedObject):
 
     def __init__(self,x_coord, y_coord, radius):
         super(CircularObject, self).__init__(x_coord, y_coord)
         self.radius = radius
-        self.shape = "Circle"
+        self.set_shape("Circle")
 
     def get_radius(self):
         return self.radius
-
-    def get_shape(self):
-        return self.shape
 
 class PolygonalObject(PlacedObject):
 
     def __init__(self):
         pass
 
-    def get_shape(self):
-        return self.shape
 
 
 # General Functionality Test
-# test1 = PlacedObject(1,2)
-# test2 = RectangularObject(1,2,3,4)
-# test3 = CircularObject(1,2,3)
-#
-# print "General Object"
-# print "x_coord: {}, y_coord: {}".format(test1.get_x_coord(),test1.get_y_coord())
-#
-# print "Rectangle Object"
-# print "x_coord: {}, y_coord: {}, width: {}, height: {}".format(test2.get_x_coord(),test2.get_y_coord(),test2.get_width(),test2.get_height())
-#
-# print "Circular Object"
-# print "x_coord: {}, y_coord: {}, radius: {}".format(test3.get_x_coord(),test3.get_y_coord(), test3.get_radius())
+test1 = PlacedObject(1,2)
+test2 = RectangularObject(1,2,3,4)
+test3 = CircularObject(1,2,3)
+
+test1.set_type("Source")
+test2.set_type("Reflector")
+
+print "General Object"
+print "shape: {}, type: {}, x_coord: {}, y_coord: {}".format(test1.get_shape(),test1.get_type(),test1.get_x_coord(),test1.get_y_coord())
+
+print "Rectangle Object"
+print "shape: {}, type: {}, x_coord: {}, y_coord: {}, width: {}, height: {}".format(test2.get_shape(),test2.get_type(),test2.get_x_coord(),test2.get_y_coord(),test2.get_width(),test2.get_height())
+
+print "Circular Object"
+print "shape: {}, type: {}, x_coord: {}, y_coord: {}, radius: {}".format(test3.get_shape(),test3.get_type(),test3.get_x_coord(),test3.get_y_coord(), test3.get_radius())
--- double_tap.py
+++ double_tap.py
@@ -4,45 +4,36 @@ from bokeh.io import curdoc
 from bokeh.events import DoubleTap
 from bokeh.models import Button, ContinuousColorMapper, BasicTicker, ColorBar
 from bokeh.models.widgets import Slider
-from bokeh.layouts import column, row
-from bokeh.models.widgets import PreText
+from bokeh.layouts import column, row, widgetbox
+from bokeh.models.widgets import PreText, RadioButtonGroup
 import Plasmon_Modeling as PM
+import PlacedObjects as PO
 import numpy as np
 
 #setting up data
 bound = 50
-coordList=[]
-sourceList = []
-source_radius_list = []
-reflector_radius_list = []
+objectList= []
 display_data= ColumnDataSource({'data' : [np.zeros((50,50))]})
-is_source = ColumnDataSource({'source' : [True], 'shape': "Circle"})
+details = ColumnDataSource({'type' : ["Source"], 'shape': ["Circle"]})
 
+#Update message section
 update_section_title = "Update Section"
 update_messages = []
 
-#setup plotting
-p = figure(title='Double click to leave a dot.',
+#Clickable_Display
+clickable_display = figure(title='Double click to leave a dot.',
            tools="tap,reset",width=700,height=700,
            x_range=(0, bound), y_range=(0, bound))
 
-#Currently having trouble implementing the ColorBar
-
-# color_mapper = ContinuousColorMapper(palette="Viridis256", low=1, high=10)
-
-d = figure(title='Display', plot_width=700, plot_height=700,
+#Output_Display
+output_display = figure(title='Display', plot_width=700, plot_height=700,
            x_range=(0, bound), y_range=(0, bound), tools='pan,wheel_zoom,box_select,reset')
 
-d.image('data', source=display_data, palette="Viridis256",
+output_display.image('data', source=display_data, palette="Viridis256",
          x=0, y=0, dw=bound, dh=bound)
 
-# color_bar = ColorBar(color_mapper=color_mapper, ticker=BasicTicker(),
-#                      label_standoff=12, border_line_color=None, location=(0,0))
-#
-# d.add_layout(color_bar, 'right')
-
 #text items
-stats = PreText(text='Update Section', width=350)
+updates = PreText(text='Update Section', width=350)
 circular_title = PreText(text='Circular Reflectors/Sources', width=350)
 rectangular_title =  PreText(text='Rectangular Reflectors/Sources', width=350)
 
@@ -51,122 +42,221 @@ circular_radius = Slider(title = "Radius",value = 2, start = 1, end = 10, step =
 rectangular_width = Slider(title = "Width",value = 2, start = 1, end = 10, step = 1)
 rectangular_height = Slider(title = "Height",value = 2, start = 1, end = 10, step = 1)
 
-#setting up the source and reflector indicators
-source = ColumnDataSource(data=dict(x=[], y=[], r = []))
-p.circle(source=source,x='x',y='y',radius = 'r', color="navy", alpha = 0.5)
+#setting up the source and reflector indicators for circles
+c_source = ColumnDataSource(data=dict(x=[], y=[], r = []))
+clickable_display.circle(source=c_source,x='x',y='y',radius = 'r', color="navy", alpha = 0.5)
 
-reflector = ColumnDataSource(data=dict(x=[], y=[], r = []))
-p.circle(source=reflector,x='x',y='y',radius = 'r', color = "red", alpha = 0.5)
+c_reflector = ColumnDataSource(data=dict(x=[], y=[], r = []))
+clickable_display.circle(source=c_reflector,x='x',y='y',radius = 'r', color = "red", alpha = 0.5)
 
-#add a dot where the click happened
+#setting up the source and reflector indicators for rectangles
+r_source = ColumnDataSource(data=dict(top=[], bottom=[], left = [], right = []))
+clickable_display.quad(source=r_source,top='top',bottom='bottom',left = 'left', right = "right", color="navy", alpha = 0.5)
+
+r_reflector = ColumnDataSource(data=dict(top=[], bottom=[], left = [], right = []))
+clickable_display.quad(source=r_reflector,top='top',bottom='bottom',left = 'left', right = "right", color = "red", alpha = 0.5)
+
+#Add sources and reflectors to certain clicked points
 def callback(event):
-    Coords=(event.x,event.y)
-    coordList.append(Coords)
-    source_or_reflector = is_source.data['Bool'][0]
-    sourceList.append(source_or_reflector)
+    coordinates = (event.x,event.y)
+    type = details.data['type'][0]
+    shape = details.data['shape'][0]
 
-    print coordList
-    print sourceList
+    print coordinates
+    print type
+    print shape
 
-    if source_or_reflector:
-        #modifying the source radius list and updating the data
-        source_radius_list.append(circular_radius.value)
-        update_data = dict(x=[coordList[i][0] for i in range(len(coordList)) if sourceList[i]], y=[coordList[i][1] for i in range(len(coordList)) if sourceList[i]], r =source_radius_list)
-        source.data = update_data
-    else:
-        #modifying the reflector radius list and updating the data
-        reflector_radius_list.append(circular_radius.value)
-        update_data = dict(x=[coordList[i][0] for i in range(len(coordList)) if not sourceList[i]], y=[coordList[i][1] for i in range(len(coordList)) if not sourceList[i]], r = reflector_radius_list)
-        reflector.data = update_data
+    #Checking the shape
+    if shape == "Rectangle":
+        input_object = PO.RectangularObject(coordinates[0],coordinates[1],rectangular_width.value, rectangular_height.value)
+        input_object.set_type(type)
+        add_rectangle(input_object, objectList)
 
-def run():
-    simulating_model()
+    elif shape == "Circle":
+        input_object = PO.CircularObject(coordinates[0],coordinates[1],circular_radius.value)
+        input_object.set_type(type)
+        add_circle(input_object, objectList)
 
-def simulating_model():
-    #so far there are no checks to see if there is an error or intersection
 
-    #source data
-    source_x = source.data['x']
-    source_y = source.data['y']
-    source_r = source.data['r']
+def add_rectangle(input_object, in_objectList):
+    search_type = input_object.get_type()
+    in_objectList.append(input_object)
 
-    #reflector data
-    reflector_x = reflector.data['x']
-    reflector_y = reflector.data['y']
-    reflector_r = reflector.data['r']
+    temp = query_object_list(search_type,"Rectangle",in_objectList)
+    x = np.array([i.get_x_coord() for i in temp])
+    y = np.array([i.get_y_coord() for i in temp])
+    w = np.array([i.get_width() for i in temp])
+    h = np.array([i.get_height() for i in temp])
 
-    sample = PM.RectangularSample(50,50)
+    top = y+h
+    bottom = y
+    left = x
+    right = x+w
 
-    update_stats('Building Model')
-    #placing reflectors
-    for i in range(len(reflector_x)):
-        sample.placeCircularReflector(reflector_x[i],reflector_y[i],reflector_r[i])
+    update_data = dict(top = top, bottom = bottom, left = left, right = right)
 
-    #placing sources
-    for i in range(len(source_x)):
-        sample.placeCircularSource(source_x[i],source_y[i],source_r[i])
+    if search_type == "Source":
+        r_source.data = update_data
+    elif search_type == "Reflector":
+        r_reflector.data = update_data
 
-    update_stats('Setting Parameters')
-    #setting arbitrary omega and sigma values
-    sigma = PM.S()
-    omega = PM.O()
-    sigma.set_sigma_values(1,10)
-    omega.set_omega_values(1,1)
+def add_circle(input_object, in_objectList):
+    search_type = input_object.get_type()
+    in_objectList.append(input_object)
 
-    update_stats('Running Simulation')
-    #running the simulation
-    sample.run(omega,sigma,density = 200)
+    temp = query_object_list(search_type,"Circle",in_objectList)
+    x = np.array([i.get_x_coord() for i in temp])
+    y = np.array([i.get_y_coord() for i in temp])
+    r = np.array([i.get_radius() for i in temp])
 
-    update_stats('Done!')
-    awa = sample.cast_solution_to_AWA()
+    update_data = dict(x=x ,y=y ,r=r)
 
-    real_part = awa[0].T
-    im_part = awa[1].T
+    if search_type == "Source":
+        c_source.data = update_data
+    elif search_type == "Reflector":
+        c_reflector.data = update_data
 
-    display_data.data = dict(data = [real_part])
+def undo_place(in_objectList):
+    search_type = in_objectList[-1].get_type()
+    search_shape = in_objectList[-1].get_shape()
+    in_objectList = in_objectList[:-1]
+
+    if search_shape == "Rectangle":
+        temp = query_object_list(search_type,"Rectangle",in_objectList)
+        x = np.array([i.get_x_coord() for i in temp])
+        y = np.array([i.get_y_coord() for i in temp])
+        w = np.array([i.get_width() for i in temp])
+        h = np.array([i.get_height() for i in temp])
+
+        top = y+h
+        bottom = y
+        left = x
+        right = x+w
+
+        update_data = dict(top = top, bottom = bottom, left = left, right = right)
+
+        if search_type == "Source":
+            r_source.data = update_data
+        elif search_type == "Reflector":
+            r_reflector.data = update_data
+
+    elif search_shape == "Circle":
+        temp = query_object_list(search_type,"Circle",in_objectList)
+        x = np.array([i.get_x_coord() for i in temp])
+        y = np.array([i.get_y_coord() for i in temp])
+        r = np.array([i.get_radius() for i in temp])
+
+        update_data = dict(x=x ,y=y ,r=r)
+
+        if search_type == "Source":
+            c_source.data = update_data
+        elif search_type == "Reflector":
+            c_reflector.data = update_data
+
+def query_object_list(in_type, in_shape, objectList):
+    return [i for i in objectList if (i.get_type()==in_type) and (i.get_shape()==in_shape)]
 
 def reset():
-
-    del coordList[:]
-    del sourceList[:]
-    del source_radius_list[:]
-    del reflector_radius_list[:]
+    del objectList[:]
     del update_messages[:]
 
     display_data.data = dict(data = [np.random.randn(50,50)])
-    source.data = dict(x=[], y=[], r = [])
-    reflector.data = dict(x=[], y=[], r = [])
+    c_source.data = dict(x=[], y=[], r = [])
+    c_reflector.data = dict(x=[], y=[], r = [])
+    r_source.data = dict(top=[], bottom=[], left = [], right = [])
+    r_reflector.data = dict(top=[], bottom=[], left = [], right = [])
 
-
-
-
-def update_stats(input_string):
+def update_updates(input_string):
     update_messages.append(input_string)
     update_message = update_section_title
     for message in update_messages[-7:]:
         update_message+='\n'
         update_message+=message
-    stats.text = str(update_message)
+    updates.text = str(update_message)
 
 def update_image(data):
     new_data = dict()
     new_data['data'] = [data]
     display_data.data = new_data
 
+#-----------------Toggling Between Different Choices-------------------#
+
 def set_place_reflector():
     new_dict = {}
-    new_dict['Bool'] = [False]
-    is_source.data = new_dict
+    new_dict['type'] = ["Reflector"]
+    new_dict['shape'] = details.data['shape']
+    details.data = new_dict
 
 def set_place_source():
     new_dict = {}
-    new_dict['Bool'] = [True]
-    is_source.data = new_dict
+    new_dict['type'] = ["Source"]
+    new_dict['shape'] = details.data['shape']
+    details.data = new_dict
 
 def set_place_circle():
+    new_dict = {}
+    new_dict['type'] = details.data['type']
+    new_dict['shape'] = ["Circle"]
+    details.data = new_dict
 
 def set_place_rectangle():
+    new_dict = {}
+    new_dict['type'] = details.data['type']
+    new_dict['shape'] = ["Rectangle"]
+    details.data = new_dict
 
+#-----------------------------------------------------------------------#
+
+#----------------------------Simulating---------------------------------#
+def run():
+    simulating_model(objectList)
+
+def simulating_model(in_objectList):
+    #so far there are no checks to see if there is an error or intersection
+    #source data
+    c_a_r = query_object_list("Reflector", "Circle", in_objectList)
+    c_a_s = query_object_list("Source", "Circle", in_objectList)
+    r_a_r = query_object_list("Reflector","Rectangle",in_objectList)
+    r_a_s = query_object_list("Source","Rectangle",in_objectList)
+
+    sample = PM.RectangularSample(50,50)
+
+    update_updates('Building Model')
+    #placing reflectors
+    for i in c_a_r:
+        sample.placeCircularReflector(i.get_x_coord(),i.get_y_coord(),i.get_radius())
+
+    for i in r_a_r:
+        sample.placeRectangularReflector(i.get_x_coord(), i.get_y_coord(), i.get_width(), i.get_height())
+
+    #placing sources
+    for i in c_a_s:
+        sample.placeCircularSource(i.get_x_coord(),i.get_y_coord(),i.get_radius())
+
+    for i in r_a_s:
+        sample.placeRectangularSource(i.get_x_coord(), i.get_y_coord(), i.get_width(), i.get_height())
+
+    update_updates('Setting Parameters')
+    #setting arbitrary omega and sigma values
+    sigma = PM.S()
+    omega = PM.O()
+    sigma.set_sigma_values(1,10)
+    omega.set_omega_values(1,1)
+
+    update_updates('Running Simulation')
+    #running the simulation
+    sample.run(omega,sigma,density = 200)
+
+    update_updates('Done!')
+    awa = sample.cast_solution_to_AWA()
+
+    real_part = awa[0].T
+    im_part = awa[1].T
+
+    display_data.data = dict(data = [real_part])
+#-----------------------------------------------------------------------#
+
+#--------------------------------Buttons--------------------------------#
 
 
 # add a button widget and configure with the call back
@@ -190,13 +280,23 @@ button5.on_click(set_place_circle)
 button6 = Button(label="Toggle Rectangle")
 button6.on_click(set_place_rectangle)
 
-p.on_event(DoubleTap, callback)
-layout=Column(p)
+def update():
+    print toggle_circle_rectangle.active
 
-toggle_source_reflector = row(button3,button4)
+# toggle_circle_rectangle = RadioButtonGroup(
+#         labels=["Rectangle", "Circle"], active=0)
+# toggle_circle_rectangle.on_change('active', lambda attr ,old, new, update())
 
-button_display = column(button1, button2, button3, button4, width = 50)
+
+#-----------------------------------------------------------------------#
+
+clickable_display.on_event(DoubleTap, callback)
+
+toggle_source_reflector = row(button3, button4)
+toggle_circle_rectangle = row(button5, button6)
+
+button_display = column(button1, button2, toggle_source_reflector,toggle_circle_rectangle, width = 50)
 circular_slider_display = column(circular_title,circular_radius)
 rectangular_slider_display = column(rectangular_title, rectangular_width, rectangular_height)
-right_display = column(button_display,circular_slider_display,rectangular_slider_display,stats)
-curdoc().add_root(row(layout,d,right_display))
+right_display = column(button_display,circular_slider_display,rectangular_slider_display,updates)
+curdoc().add_root(row(clickable_display,output_display,right_display))
