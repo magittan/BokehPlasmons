454e3b8cfb204105cd9bddc91dbd6b8278a12906
meberko <slashx14@gmail.com>
2019-10-23 10:31:50 -0400

    got GPU method working, anywhere from 20-500x speedup depending on number of eigenfunctions used.

--- BasisChangeTest.py
+++ BasisChangeTest.py
@@ -1,9 +1,10 @@
-import h5py,time,os
+import h5py,time,os, math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import jv
 from scipy.integrate import nquad
 from numba import jit, cuda, prange
+
 
 def Progress(i,L,last):
     next = last+10
@@ -46,23 +47,22 @@ def ChangeBasisCPU(to_eigenbasis,from_eigenbasis,xs,ys):
     return U
 
 @cuda.jit
-def ChangeBasisGPU(U,to_eigenbasis,from_eigenbasis,xs,ys):
+def ChangeBasisGPU(U, to_eigenbasis,from_eigenbasis,xs,ys):
+    row, col = cuda.grid(2)
     dx = xs[1]-xs[0]
     dy = ys[1]-ys[0]
-    to_nn = range(to_eigenbasis.shape[0])
-    from_nn = range(from_eigenbasis.shape[0])
     sx, sy = to_eigenbasis[0].shape
-
-    for to_n in to_nn:
-        for from_n in from_nn:
-            sum = 0
-            from_ef = to_eigenbasis[to_n]
-            to_ef = from_eigenbasis[from_n]
-            for i in range(from_ef.shape[0]):
-                for j in range(from_ef.shape[1]):
-                    sum += from_ef[i,j]*to_ef[i,j]*dx*dy
-            U[to_n,from_n] = sum
-    return U
+    if row < U.shape[0] and col < U.shape[1]:
+        sum = 0.
+        to_ef = to_eigenbasis[row]
+        from_ef = from_eigenbasis[col]
+        for i in range(from_ef.shape[0]):
+            for j in range(from_ef.shape[1]):
+                sum += from_ef[i,j]*to_ef[i,j]*dx*dy
+        cuda.syncthreads()
+        U[row,col] = sum
+        #U.append(row)
+    #return U
 
 def ImshowAll(eigenbasis):
     N = eigenbasis.shape[0]
@@ -76,12 +76,37 @@ def ImshowAll(eigenbasis):
                 ax[i,j].imshow(eigenbasis[idx],origin='lower',extent=[-1,1,-1,1])
     plt.show()
 
+def ShowSampleAndTip(sample_m,tip_m,origin='lower',extent=None):
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(sample_m,origin=origin,extent=extent)
+    plt.title('Sample Basis')
+    plt.colorbar()
+    plt.subplot(122)
+    plt.imshow(tip_m,origin=origin, extent = extent)
+    plt.title('Tip Basis')
+    plt.colorbar()
+    plt.show()
+
 def VInBasis(vec,basis):
     basis_shape = basis[0].shape
     sum = np.zeros(basis_shape)
     for i,vi in enumerate(vec):
         sum += vi*basis[i]
     return sum
+
+def ConstructEigenbases(N_tip_eigenfunctions,N_sample_eigenfunctions,rs,theta,sample_eigenvalues,sample_eigenpairs):
+    tip_eigenbasis, sample_eigenbasis = [],[]
+
+    for n in range(N_sample_eigenfunctions):
+        sample_eigenbasis.append(sample_eigenpairs[sample_eigenvalues[n]])
+
+    for alpha in range(N_tip_eigenfunctions):
+        tip_eigenbasis.append(jv(alpha//10,10*rs)*np.cos((alpha%10)*theta))
+
+    sample_eigenbasis = np.asarray(sample_eigenbasis)
+    tip_eigenbasis = np.asarray(tip_eigenbasis)
+    return tip_eigenbasis,sample_eigenbasis
 
 def IntFun(fun,x,y):
     x = int((x+1)*50)
@@ -104,81 +129,74 @@ def OneHot(i,L):
 def main():
     sample_eigenpairs = LoadEigenbasis('UnitSquareMesh_100x100_1000_eigenbasis.h5')
     sample_eigenvalues = sorted(list(sample_eigenpairs.keys()))
+    TPB = 16
 
     extent = [-1,1,-1,1]
+
     nx, ny=101,101
-    xs = np.linspace(-1,1,nx)
-    ys = np.linspace(-1,1,ny)
+    xs, ys = np.linspace(-1,1,nx), np.linspace(-1,1,ny)
     xv,yv = np.meshgrid(xs,ys,sparse=True)
+
     rs = np.sqrt(xv**2+yv**2)
     theta = np.arctan(yv/xv)
     theta[np.where(np.isnan(theta))] = 0
     times = {}
 
     #for n in range(200):
-    for n in [100]:
-        N_tip_eigenfunctions = n # Dimension of tip eigenbasis; in this case we use bessel functions
-        #N_sample_eigenfunctions = len(sample_eigenvalues)
-        N_sample_eigenfunctions = 1000
+    for N_tip_eigenfunctions in [100]:
+        for N_sample_eigenfunctions in [1000]:
+            # Construct tip and sample eigenbases
+            tip_eigenbasis, sample_eigenbasis = ConstructEigenbases(N_tip_eigenfunctions,N_sample_eigenfunctions,rs,theta,sample_eigenvalues,sample_eigenpairs)
+            #ImshowAll(tip_eigenbasis)
 
-        # Construct tip and sample eigenbases
-        tip_eigenbasis, sample_eigenbasis = [], []
-        for n in range(N_sample_eigenfunctions):
-            sample_eigenbasis.append(sample_eigenpairs[sample_eigenvalues[n]])
+            # Construct change of basis matrix U
+            start = time.time()
+            U_tip_to_sample = ChangeBasis(sample_eigenbasis,tip_eigenbasis,xs,ys)
+            unopt_elapsed = time.time()-start
+            print("Basis Change (tip to sample): {} seconds".format(unopt_elapsed))
 
-        for alpha in range(N_tip_eigenfunctions):
-            tip_eigenbasis.append(jv(alpha//10,10*rs)*np.cos((alpha%10)*theta))
+            threadsperblock = (TPB,TPB)
+            blockspergrid_x = int(math.ceil(N_tip_eigenfunctions/threadsperblock[0]))
+            blockspergrid_y = int(math.ceil(N_sample_eigenfunctions/threadsperblock[0]))
+            blockspergrid = (blockspergrid_x,blockspergrid_y)
 
-        sample_eigenbasis = np.asarray(sample_eigenbasis)
-        tip_eigenbasis = np.asarray(tip_eigenbasis)
-        #ImshowAll(tip_eigenbasis)
+            d_sample_eigenbasis = cuda.to_device(sample_eigenbasis)
+            d_tip_eigenbasis = cuda.to_device(tip_eigenbasis)
+            d_U = cuda.device_array((N_sample_eigenfunctions,N_tip_eigenfunctions))
 
-        # Construct change of basis matrix U
-        start = time.time()
-        U_tip_to_sample = ChangeBasis(sample_eigenbasis,tip_eigenbasis,xs,ys)
-        #U_sample_to_tip = ChangeBasis(tip_eigenbasis,sample_eigenbasis,xs,ys)
-        elapsed = time.time()-start
-        print("Basis Change (tip to sample): {} seconds".format(elapsed))
+            start = time.time()
+            ChangeBasisGPU[blockspergrid,threadsperblock](d_U,d_sample_eigenbasis,d_tip_eigenbasis,xs,ys)
+            GPU_elapsed = time.time()-start
+            print("Basis Change (tip to sample) with GPU: {} seconds".format(GPU_elapsed))
+            print("Speedup: {}".format(unopt_elapsed/GPU_elapsed))
+            res = d_U.copy_to_host()
+            #ShowSampleAndTip(U_tip_to_sample,res)
+            times[N_tip_eigenfunctions*N_sample_eigenfunctions] = unopt_elapsed/GPU_elapsed
 
-        start = time.time()
-        U_tip_to_sample = ChangeBasisCPU(sample_eigenbasis,tip_eigenbasis,xs,ys)
-        elapsed = time.time()-start
-        print("Basis Change (tip to sample) with CPU: {} seconds".format(elapsed))
-
-        U_tip_to_sample = np.zeros(N_tip_eigenfunctions,N_sample_eigenfunctions)
-        start = time.time()
-        U_tip_to_sample = ChangeBasisGPU(U_tip_to_sample,sample_eigenbasis,tip_eigenbasis,xs,ys)
-        elapsed = time.time()-start
-        print("Basis Change (tip to sample) with GPU: {} seconds".format(elapsed))
-
-        times[N_tip_eigenfunctions*N_sample_eigenfunctions] = elapsed
-
-    tip_v = OneHot(0,N_tip_eigenfunctions)+OneHot(1,N_tip_eigenfunctions)
+    tip_v = OneHot(0,N_tip_eigenfunctions)+OneHot(1,N_tip_eigenfunctions)+OneHot(2,N_tip_eigenfunctions)
     tip_m = VInBasis(tip_v,tip_eigenbasis)
     sample_v = U_tip_to_sample.dot(tip_v.T)
+    sample_v_GPU = res.dot(tip_v.T)
     sample_m = VInBasis(sample_v,sample_eigenbasis)
+    sample_m_GPU = VInBasis(sample_v_GPU,sample_eigenbasis)
     """
     sample_v = OneHot(0,N_sample_eigenfunctions)+OneHot(1,N_sample_eigenfunctions)
     sample_m = VInBasis(sample_v,sample_eigenbasis)
     tip_v = U_sample_to_tip.dot(sample_v.T)
     tip_m = VInBasis(tip_v,tip_eigenbasis)
-    """
 
-    #max_ratio = np.max(tip_m)/np.max(sample_m)
     sample_m = sample_m/np.max(sample_m)
+    sample_m_GPU = sample_m_GPU/np.max(sample_m_GPU)
     tip_m = tip_m/np.max(tip_m)
     diff = np.sum((sample_m-tip_m)**2)/np.prod(sample_m.shape)
+    res_norm = res /np.max(res)
+    U_tip_to_sample_norm = U_tip_to_sample /np.max(U_tip_to_sample)
+    diff = np.sum((res_norm-U_tip_to_sample_norm)**2)/np.prod(res_norm.shape)
     print('Difference: {}'.format(diff))
+    """
 
-    plt.figure()
-    plt.subplot(121)
-    plt.imshow(sample_m,origin='lower',extent=extent)
-    plt.title('Sample Basis')
-    plt.colorbar()
-    plt.subplot(122)
-    plt.imshow(tip_m,origin='lower', extent = extent)
-    plt.title('Tip Basis')
-    plt.colorbar()
-    plt.show()
+    #ShowSampleAndTip(U_tip_to_sample[:10,:10],res[:10,:10],origin='upper')
+    ShowSampleAndTip(sample_m_GPU,tip_m)
+    ShowSampleAndTip(sample_m,tip_m)
 
 if __name__=='__main__': main()
